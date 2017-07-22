0e8df17dc303f16a92af92e1efa08be60baca664
Collin Chin <collin.chin@berkeley.edu>
2017-07-22 02:09:24 +0200

    CHECK THE README

--- contracts/C.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.10;
-contract C {
-		function blah (uint a) constant returns(uint){
-			return a+5;
-		}
-    function a() constant returns (uint) {
-        return block.gaslimit;
-    }
-}
