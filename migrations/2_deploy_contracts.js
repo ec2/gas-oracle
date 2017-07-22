b218c8f989b02fc9e8652945eab6fa7e83feac31
naterush <narush@wharton.upenn.edu>
2017-07-22 16:21:36 -0400

    add basic test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var GasHole = artifacts.require("./GasHole.sol");
+var Database = artifacts.require("./Database.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+
 };
--- test/metacoin.js
+++ test/metacoin.js
@@ -1,7 +1,16 @@
-var MetaCoin = artifacts.require("./Database.sol");
+var Database = artifacts.require("./Database.sol");
+var GasHole = artifacts.require("./GasHole.sol");
 
 contract('Database', function(accounts) {
-  it("should allow for block submission", function () {
-
+  it("should deploy contracts", function () {
+    var db
+    var gasHole
+    Database.new().then(instance => {
+      db = instance
+      return GasHole.new(db.address)
+    }).then(instance => {
+      gasHole = instance
+      //do shit
+    })
   })
 })
